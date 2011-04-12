97d9daf09df01b2c454934bad05b88e110ea9a46
Joel Hansson <joel.hansson@gmail.com>
2011-04-12 21:31:32 +0200

    made the require_libs safer, and worked on the bootstrap sample

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,10 @@
 <?php
 define('PROJECT_PATH', dirname(__FILE__));
-require PROJECT_PATH . '/coltrane/coltrane.php';
+if (is_dir(PROJECT_PATH . '/coltrane')) {
+	require PROJECT_PATH . '/coltrane/coltrane.php';
+} else {
+	require 'coltrane/coltrane.php';
+}
+if (is_file(PROJECT_PATH . '/config.php')) {
+	require PROJECT_PATH . '/config.php';
+}
--- coltrane/coltrane.php
+++ coltrane/coltrane.php
@@ -2,12 +2,13 @@
 if ( ! defined('PROJECT_PATH') ) die('You need to specify PROJECT_PATH');
 define('COLTRANE_PATH', dirname(__FILE__));
 session_start(); // @todo make this configurable
-require PROJECT_PATH.'/config.php';
 
 $GLOBALS['_required_files'] = array();
 function require_libs($path = '')
 {
-	foreach ( glob( $path.'/*' ) as $p ) {
+	$glob_array = glob( $path . '/*' );
+	if (empty($glob_array)) return false;
+	foreach ( $glob_array as $p ) {
 		if (is_dir($p)) {
 			require_libs( $p );
 		} else if ( is_file($p) && substr(basename($p),-4,4) == '.php' && ! in_array($p, $GLOBALS['_required_files'])) {
